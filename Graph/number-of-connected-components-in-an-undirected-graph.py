0fd33536dac80835e7c76de1c340bcf3202199e2
HarutoshiOkumura <HOkumura032@gmail.com>
2025-06-11 16:57:52 -0400

    Have not completed no.-of-connected-components.py yet

---
+++ Graph/number-of-connected-components-in-an-undirected-graph.py
@@ -0,0 +1,12 @@
+"""
+You have a graph of n nodes. You are given an integer n and an array edges where edges[i] = [ai, bi] indicates that there is an edge between ai and bi in the graph.
+
+Return the number of connected components in the graph.
+
+Input: n = 5, edges = [[0,1],[1,2],[3,4]]
+Output: 2
+"""
+
+class Solution:
+    def countComponents(self, n: int, edges: List[List[int]]) -> int:
+        
